903f53c7e88e290ad2e85b32333f8e5eed3c5880
Joe Ladson <jladson@amplify.com>
2017-03-28 12:02:14 -0400

    changes

--- python/ex4.py
+++ python/ex4.py
@@ -1,5 +1,9 @@
 print "There are", (50 + 50), "cars available."
 print "There are only", (35 - 5), "drivers available."
-# print "We can transport", carpool_capacity, "people today."
-# print "We have", passengers, "to carpool today."
-# print "We need to put about", average_passengers_per_car, "in each car."
+print "There will be", (30 + 40), "empty cars today."
+print "We can transport", 60.0 + 60.0, "people today."
+print "We have", 45 + 45, "to carpool today."
+print "We need to put about", 10 - 5 - 2, "in each car."
+
+print "5 is greater than 20", 5 > 20
+print "5 is greater than or equal to 5", 5 >= 5
