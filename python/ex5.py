10dd0253a5f0160e17b7f5b082c92a73ffc26c1f
Joe Ladson <jladson@amplify.com>
2017-04-10 15:58:09 -0400

    python changes

---
+++ python/ex5.py
@@ -0,0 +1,10 @@
+print "Let's talk about 'Zed A. Shaw'"
+print "He's", (40-5), #"not a lie."
+print "He's", (70+4), "inches tall."
+print "He's", (90+90), "pounds heavy."
+print "Actually that's not too heavy."
+print "He's got 'blue' eyes and 'Brown' hair."
+print "His teeth are usually 'White' depending on the coffee."
+
+# this line tricky, try to get it exactly right
+print "If I add", (40-5), (70+4), "and", (90+90), "I get", (145+144), "."
