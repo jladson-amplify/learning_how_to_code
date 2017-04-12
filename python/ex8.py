4c031c3947305b148ac20c6865fa982311e34ed3
Joe Ladson <jladson@amplify.com>
2017-04-11 21:28:44 -0400

    ex8 python

---
+++ python/ex8.py
@@ -0,0 +1,9 @@
+print ("1, 2, 3, 4")
+print ("one", "two", "three", "Four" )
+print ("True", "False", "False", "True")
+print ("%r", "%r", "%r", "%r")
+print ("I had this thing.",
+       "That you could type up right.",
+       "But it didn't sing.",
+       "So I said goodnight."
+       )
