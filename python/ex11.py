e9ebd45f0a70b82b35be8cdc4f129300b7a8adc1
Joe Ladson <jladson@amplify.com>
2017-04-13 11:34:41 -0400

    ex11 commit

---
+++ python/ex11.py
@@ -0,0 +1,11 @@
+print "How old are you?",
+age = raw_input()
+
+print "What's your name",
+name = raw_input()
+
+print "\nSo, you're %r, and %r years old" %(name, age)
+
+print "Hi %r" % name
+
+print "\nSo, you're %r, and %r years old\n Hi %r" % (name, age, name)
