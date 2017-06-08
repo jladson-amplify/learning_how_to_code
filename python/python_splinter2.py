3f1cd04255c0e88166381411ad36a77b923f4783
Joe Ladson <jladson@amplify.com>
2017-06-08 15:36:09 -0400

    commiting python splinter automation 2

--- python/python_splinter2.py
+++ python/python_splinter2.py
@@ -56,6 +56,39 @@ with Browser("chrome") as browser:
 
     button = browser.find_link_by_text('Assessment')
     button.click()
+    if browser.is_text_present("Amplify"):
+        print("Yes, the official Amplify website was seen!")
+    else:
+        print("No, it wasn't found... Try Try Again")
 
 
-    #browser.find_by_text('#pop-up')
+    button = browser.find_link_by_href('/company')
+    button.click()
+    if browser.is_text_present("Company"):
+        print("Yes, the official Amplify website is displaying correctly!")
+    else:
+        print("No, it wasn't found... Please Try Try Again")
+
+
+    button = browser.find_link_by_href('/leadership')
+    button.click()
+    if browser.is_text_present("Amplify"):
+        print("If Correct, lebron will win the championship")
+    else:
+        print("No, it wasn't found... You lose -_-")
+
+
+    button = browser.find_link_by_href('/partners')
+    button.click()
+    if browser.is_text_present("Clever"):
+        print("If Correct, partners are important to have")
+    else:
+        print("No, it wasn't found... Partners are not around")
+
+
+    button = browser.find_link_by_href('/newsroom')
+    button.click()
+    if browser.is_text_present("announcement"):
+        print("If Correct, we will be in the CNN newsroom")
+    else:
+        print("No, it wasn't found... The newsrooms are closed")
