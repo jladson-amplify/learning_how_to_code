12d7affc3df4abc66b041086e5fde89653b5090b
Joe Ladson <jladson@amplify.com>
2017-06-21 16:16:01 -0400

    python automated

--- python/python_splinter3.py
+++ python/python_splinter3.py
@@ -19,3 +19,32 @@ with Browser("chrome") as browser:
 
     button = browser.find_by_css(".rooms-update")[1]
     print (repr(button))
+
+    browser.back()
+
+    browser.visit(url)
+    button = browser.find_link_by_href("http://www.phptravels.net/hotels/switzerland/messe-basel/Swissotel-Le-Plaza-Basel")
+    button.click()
+    if browser.is_text_present("Swissotel Le Plaza Basel"):
+        print("Yes, the official website was found!")
+    else:
+        print("No, it wasn't found... Try Try Again")
+
+    button = browser.find_by_css(".rooms-update")[0]
+    print (repr(button))
+
+    browser.back()
+
+    browser.visit(url)
+    button = browser.find_link_by_href("http://www.phptravels.net/hotels/united-arab-emirates/dubai/Hyatt-Regency-Perth").third
+    button.click()
+    if browser.is_text_present("Hyatt Regency Perth"):
+        print("Yes, the official website was found!")
+    else:
+        print("No, it wasn't found... Try Try Again")
+
+    button = browser.find_by_css(".rooms-update")[2]
+    print (repr(button))
+
+
+#    print (repr(button))
